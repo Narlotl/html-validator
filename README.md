f419bbe2f271ab3248e4dd9b1bac416f9e333ba1
Narlotl <fretwellcalifornia@gmail.com>
2024-01-10 10:29:45 -0800

    v2.0.0

--- README.md
+++ README.md
@@ -2,40 +2,51 @@
 
 ## Features
 
-Uses the [W3C Markup Validation Service](https://validator.w3.org/nu#textarea) to check HTML for issues like extra closing tags or bad practice.
+Uses the [Nu Html Checker](https://validator.github.io/validator/) to check HTML for issues like extra closing tags or bad practice.
 
 Run ```Check HTML``` to validate.
-To ignore an error, copy its ID from the results tab and paste into the ```html-validator.ignore``` array in ```settings.json```.
 
-> ![Example](https://raw.githubusercontent.com/Narlotl/html-validator/main/example.gif "Example")
+> ![Example](example.gif "Example")
+
+## Requirements
+
+HTML Checker requires [Java 8+](https://www.java.com/en/download/) as of v2.0.0. If you don't want to install Java, install the extension at v1.3.1.
 
 ## Release Notes
 
-### 1.0.0
+### 2.0.0
 
-Initial release of HTML Checker.
+- HTML Checker now runs locally with the [Nu Html Checker](https://validator.github.io/validator/) and requires Java.
+- Instead of a webview, errors and warnings now show up as errors in the file
+- Validation can now be done when a file is saved with the `html-validator: Validate-on-save` setting
+- Removed the status bar button
+- You can no longer filter errors
 
-### 1.0.1
+### 1.3.1
 
-Only allow HTML files.
-
-### 1.1.0
-
-Filter code from static site generators.
-
-### 1.2.0
-
-Allow ignoring of specific errors through settings.
-
-### 1.2.1
-
-More static site generator filtering.
+Allow adding file associations other than HTML through settings.
 
 ### 1.3.0
 
 Added key command and status bar item to run command.
 Press `alt+shift+c` to check HTML.
 
-### 1.3.1
+### 1.2.1
 
-Allow adding file associations other than HTML through settings.
+More static site generator filtering.
+
+### 1.2.0
+
+Allow ignoring of specific errors through settings.
+
+### 1.1.0
+
+Filter code from static site generators.
+
+### 1.0.1
+
+Only allow HTML files.
+
+### 1.0.0
+
+Initial release of HTML Checker.
--- extension.js
+++ extension.js
@@ -1,99 +1,67 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const vnu = require('vnu-jar');
+const { execFile } = require('child_process');
+
+const workspaceConfig = vscode.workspace.getConfiguration('html-validator');
+const diagnosticCollection = vscode.languages.createDiagnosticCollection('html-validator');
+const validate = /** @param {vscode.TextDocument} document @param {boolean} fromSave */ (document, fromSave) => {
+	if (!workspaceConfig.get('validate-on-save') && fromSave)
+		return;
+	if (workspaceConfig.get('file-types').includes(document.languageId)) {
+		vscode.window.setStatusBarMessage('Checking HTML...');
+
+		const process = execFile('java', ['-jar', '"' + vnu + '"', '--html', '--format json', '-'], { shell: true }, (error, stdout, stderr) => {
+			const data = JSON.parse(stderr).messages;
+
+			diagnosticCollection.clear();
+			const diagnostics = [];
+			let errorLevel = false;
+			for (const err of data) {
+				if (err.type === 'error')
+					errorLevel = true;
+				diagnostics.push(new vscode.Diagnostic(new vscode.Range((err.firstLine || err.lastLine) - 1, err.firstColumn, err.lastLine - 1, err.lastColumn), err.message, err.type === 'error' ? vscode.DiagnosticSeverity.Error : vscode.DiagnosticSeverity.Warning));
+			}
+			diagnosticCollection.set(document.uri, diagnostics);
+
+			if (!fromSave) {
+				const message = 'HTML Validator: ' + data.length + ' error' + (data.length == 1 ? '' : 's') + ' found';
+				if (data.length == 0)
+					vscode.window.showInformationMessage(message);
+				else if (errorLevel)
+					vscode.window.showErrorMessage(message);
+				else
+					vscode.window.showWarningMessage(message);
+			}
+
+			vscode.window.setStatusBarMessage('');
+		});
+
+		let text = document.getText().replaceAll(/{%.*%}|{{.*}}/gm, '');
+		if (text.indexOf('---') == 0)
+			text = text.replace(/---(.|\n)*---/gm, '');
+		process.stdin.write(text);
+		process.stdin.end();
+	}
+	else if (!fromSave)
+		vscode.window.showErrorMessage('Please open an HTML file');
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('html-validator.validate', function () {
-		// The code you place here will be executed every time your command is executed
-
-		if (vscode.window.activeTextEditor) {
-			if (vscode.workspace.getConfiguration('html-validator').get('file-types').includes(vscode.window.activeTextEditor.document.languageId)) {
-				vscode.window.setStatusBarMessage('Checking HTML...');
-
-				function onreadystatechange() {
-					if (req.status == 200 && req.readyState == 4) {
-						const panel = vscode.window.createWebviewPanel(
-							'html-checker',
-							'HTML Checker results',
-							vscode.ViewColumn.Active,
-							{}
-						);
-						let data = [req.responseText];
-						data = data[0].substring(data[0].indexOf('<ol>') + 4, data[0].indexOf('</ol>')).split('</li>');
-						if (data.length > 1) {
-							data.forEach(error => {
-								if (vscode.workspace.getConfiguration('html-validator').get('ignore').includes(error.split('').reduce((prevHash, currVal) =>
-									(((prevHash << 5) - prevHash) + currVal.charCodeAt(0)) | 0, 0)))
-									data.splice(data.indexOf(error), 1);
-							});
-							panel.webview.html = `
-							<style>
-								.error strong {
-									background-color:rgb(255,100,100);
-								}
-									
-								.warning strong {
-									background-color:rgb(250,151,58);
-								}
-							</style>
-							<ul>
-							<h1>HTML Checker results</h1>
-							<h2>` + (data.length - 1) + ' issues found</h2>';
-							data.pop();
-							data.forEach(error => {
-								panel.webview.html += error + '<small><i>ID: ' + error.split('').reduce((prevHash, currVal) =>
-									(((prevHash << 5) - prevHash) + currVal.charCodeAt(0)) | 0, 0) + '</i></small></li>'
-							});
-							panel.webview.html += '</ul>'
-							this.resolve();
-						}
-						else
-							panel.webview.html = '<h1>No errors found!</h1>';
-					}
-					else if (req.readyState == 4) {
-						vscode.window.showErrorMessage(req.statusText);
-						this.reject();
-					}
-
-					vscode.window.setStatusBarMessage('');
-				}
-
-				const req = new XMLHttpRequest();
-				req.open('POST', 'https://validator.w3.org/nu/#textarea');
-				req.onreadystatechange = onreadystatechange;
-				req.setRequestHeader('Content-Type', 'text/html');
-				let text = vscode.window.activeTextEditor.document.getText().replaceAll(/{%.*%}|{{.*}}/gm, '');
-				if (text.indexOf('---') == 0)
-					text = text.replace(/---(.|\n)*---/gm, '');
-				req.send(text);
-			}
-			else
-				vscode.window.showErrorMessage('Please open an HTML file');
-		}
+		if (vscode.window.activeTextEditor)
+			validate(vscode.window.activeTextEditor.document, false);
 		else
 			vscode.window.showErrorMessage('No active text editor');
 	});
 
+	vscode.workspace.onDidSaveTextDocument(document => validate(document, true));
+
 	context.subscriptions.push(disposable);
-	const sb = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
-	context.subscriptions.push(sb);
-	sb.text = '$(file-code) Check HTML';
-	sb.tooltip = 'Check HTML';
-	sb.command = 'html-validator.validate';
-	sb.show();
 }
 
-// this method is called when your extension is deactivated
 function deactivate() { }
 
 module.exports = {
